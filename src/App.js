d3287632d673b51210d20ab05142d074a9bbd6ab
shubham hirap <shirap07@gmail.com>
2020-12-11 11:39:32 +0530

    widgets app completed with routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Translate from "./components/Translate";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -32,6 +33,7 @@ const App = () => {
 
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
